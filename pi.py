880f21bc1cf7e6efcf1262bc9157da868dc87c86
Code Monkey King <freesoft.for.people@gmail.com>
2022-08-03 19:47:51 +0300

    added access to editor's variables from the config file

--- pi.py
+++ pi.py
@@ -1,7 +1,7 @@
 #!/bin/python3
 import curses, json, sys, os, time
 with open('commands.json') as f: com = json.loads(f.read())
-cx, cy = [0] * 2; b = []; src = ''
+cur = [0, 0]; b = []; src = ''; ch = ''; s = None
 try:
   with open(sys.argv[1]) as f: content = f.read().split('\n')
   for rw in content[:-1]: b.append([ord(c) for c in rw])
@@ -9,7 +9,7 @@ try:
   src = sys.argv[1]
 except: b.append([]); b.append([])
 def main(stdscr):
-  global cx, cy, b
+  global R, C, cur, b, ch, s, r, c, x, y
   s = curses.initscr()
   s.keypad(True)
   s.nodelay(1)
@@ -36,15 +36,15 @@ def main(stdscr):
     s.move(r - y, c - x) if m else s.move(R, c - x)
     curses.curs_set(1); s.refresh(); ch = -1;
     while (ch == -1): ch = s.getch()
-    [exec(com[key]['exec'], globals().update({'ch': ch})) for key in com.keys() if eval(com[key]['bind'])]
+    [exec(com[key]['exec'], globals()) for key in com.keys() if eval(com[key]['bind'])]; 
     if ch == curses.KEY_RESIZE: R, C = s.getmaxyx(); R -= 1; s.refresh(); y = 0
-    if ((ord('x')) & 0x1f) == ch: m ^= 1; cy = r; cx = c; r = len(b)-1; c = 0;
+    if ((ord('x')) & 0x1f) == ch: m ^= 1; cur[0] = r; cur[1] = c; r = len(b)-1; c = 0;
     if ((ch) & 0x1f) != ch and ch < 128: b[r].insert(c, ch); c += 1
     if ch == ord('\n') and not m:
       m ^= 1;
       try: exec(''.join([chr(i) for i in b[-1]]), globals());
       except Exception as e: s.move(R, 0); s.addstr(str(e)); s.refresh(); time.sleep(3);
-      b[-1] = []; r = cy; c = cx; continue
+      b[-1] = []; r = cur[0]; c = cur[1]; continue
     if ch == ord('\n'): l = b[r][c:]; b[r] = b[r][:c]; r += 1; c = 0; b.insert(r, [] + l)
     if ch == curses.KEY_BACKSPACE and c: c -= 1; del b[r][c]
     elif ch == curses.KEY_BACKSPACE and c == 0 and r and m: l = b[r][c:]; del b[r]; r -= 1; c = len(b[r]); b[r] += l
